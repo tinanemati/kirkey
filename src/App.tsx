f9ec82d424f8a6b379a7538e78c603d3c1bf7672
tinanemati <tinanemati.tina@gmail.com>
2024-04-17 15:38:47 -0700

    added the component that will show the name of authors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Navbar from './components/layout/Navbar'
+import AuthorList from './components/AuthorList'
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>hello!</h1>
+      <AuthorList />
     </>
   )
 }
---
+++ src/components/AuthorList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CancelIcon from "@mui/icons-material/Cancel";
+import "./AuthorList.css";
+
+const AuthorList: React.FC = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="container-header">
+          <ArrowBackIcon />
+          <h4>You have Top 10 Authors</h4>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuthorList;
