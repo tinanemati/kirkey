66af12bf46adb78cf11cc253f3fe322c5b283d33
tinanemati <tinanemati.tina@gmail.com>
2024-04-18 01:48:39 -0700

    fixed the bug in correctly connecting to database

--- server/db.js
+++ server/db.js
@@ -1,8 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import pg from 'pg';
+const { Pool } = pg;
 
-const supabaseUrl = "https://tjexdoxoedoaccfqfizr.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZXhkb3hvZWRvYWNjZnFmaXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzODQ0MDgsImV4cCI6MjAyODk2MDQwOH0.3oAKVJpp_Ohm-N8orsiCCNeXZQDngx981qimL3gWBDo";
-const supabase = createClient(supabaseUrl, supabaseKey);
+// Create a new pool using the connection string for your local PostgreSQL database
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'mydatabase',
+  port: 5432,
+});
 
-export default supabase;
+export default pool;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,10 @@
 import express from "express";
 import cors from "cors";
-import supabase from "./db.js";
+import pool from "./db.js"
 import NodeCache from "node-cache";
 import rateLimit from "express-rate-limit";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBMrFNYBaR7YGYueMVgN45SFUqllyccgDQ",
-  authDomain: "krikey-challenge-3179e.firebaseapp.com",
-  projectId: "krikey-challenge-3179e",
-  storageBucket: "krikey-challenge-3179e.appspot.com",
-  messagingSenderId: "143691344919",
-  appId: "1:143691344919:web:31c1207e9ff3d1728c4052",
-  measurementId: "G-MKM8KYEP4F"
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const expressApp = express();
+const app = express();
 const myCache = new NodeCache();
 
 // middleware
@@ -81,8 +60,11 @@ app.get("/top-authors", async (req, res) => {
       `;
       queryParams = [author_name];
     }
-
+    console.log("I am about to call database")
+    console.log("This is my query", query)
+    console.log("This is my queryParams", queryParams)
     const { rows } = await pool.query(query, queryParams);
+    console.log("this is rows:", rows)
     if (rows.length === 0) {
       return res
         .status(404)
